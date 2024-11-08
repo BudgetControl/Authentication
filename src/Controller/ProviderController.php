7c128cef29c473c3fb2cf86d97879667c1646cb6
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2024-11-08 11:56:06 +0100

    added tests

--- src/Controller/ProviderController.php
+++ src/Controller/ProviderController.php
@@ -30,7 +30,7 @@ class ProviderController {
         try {
 
             $authCognito = Facade::getFacadeApplication()["aws-cognito-client"];
-            if($queryParams['mobile'] === 'android' || $queryParams['mobile'] === 'ios') {
+            if($queryParams['device'] === 'android' || $queryParams['device'] === 'ios') {
                 $authCognito = $authCognito->setAppRedirectUri(env('AWS_COGNITO_REDIRECT_DEEPLINK'));
             }
 
